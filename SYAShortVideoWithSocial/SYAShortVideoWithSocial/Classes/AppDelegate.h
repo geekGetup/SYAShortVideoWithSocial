29071d3ace63af03a2b03a59971043984e5366c0
123456 <1044212178@qq.com>
2017-11-11 15:48:56 +0800

    项目初始化--SYA

---
+++ SYAShortVideoWithSocial/SYAShortVideoWithSocial/Classes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SYAShortVideoWithSocial
+//
+//  Created by 乐家 on 2017/11/11.
+//  Copyright © 2017年 lejiakeji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SYAShortVideoWithSocial/SYAShortVideoWithSocial/Classes/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SYAShortVideoWithSocial
+//
+//  Created by 乐家 on 2017/11/11.
+//  Copyright © 2017年 lejiakeji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
